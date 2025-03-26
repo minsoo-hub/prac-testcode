cea3d6e0acc04163a237f55aea2b305f749f04ff
naminsu <naminsoo1020@naver.com>
2025-03-27 00:51:45 +0900

    feat: 카페 오픈&마감 시간 추가 및 키오스크 주문 생성 시, LocalDateTime을 매개변수로 받을 때, 안받을 때 이유: test시 오픈 마감 시간에 따른 테스트가 통과하지 않아서 수정

--- src/main/java/sample/practestcode/unit/CafeKioskRunner.java
+++ src/main/java/sample/practestcode/unit/CafeKioskRunner.java
@@ -2,17 +2,22 @@ package sample.practestcode.unit;
 
 import sample.practestcode.unit.beverage.Americano;
 import sample.practestcode.unit.beverage.Latte;
+import sample.practestcode.unit.order.Order;
+
+import java.time.LocalDateTime;
 
 public class CafeKioskRunner {
 
     public static void main(String[] args) {
-        CafeKiosk kiosk = new CafeKiosk();
-        kiosk.add(new Americano());
+        CafeKiosk cafeKiosk = new CafeKiosk();
+        cafeKiosk.add(new Americano());
         System.out.println(">>>> 아메리카노 추가");
-        kiosk.add(new Latte());
+        cafeKiosk.add(new Latte());
         System.out.println(">>>> 라떼 추가");
 
-        int totalPrice = kiosk.calculateTotalPrice();
+        int totalPrice = cafeKiosk.calculateTotalPrice();
         System.out.println("totalPrice = " + totalPrice);
+
+        Order order = cafeKiosk.createOrder(LocalDateTime.now());
     }
 }

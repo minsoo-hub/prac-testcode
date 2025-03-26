cbda3cd13fb9dd97b4a5abb5b059ae1a37d5e277
naminsu <naminsoo1020@naver.com>
2025-03-27 00:47:43 +0900

    test: 여러음료추가 성공&예외 테스트, 오픈&마감시간 성공&예외 테스트

--- src/test/java/sample/practestcode/unit/beverage/AmericanoTest.java
+++ src/test/java/sample/practestcode/unit/beverage/AmericanoTest.java
@@ -10,14 +10,14 @@ class AmericanoTest {
     @Test
     void getName() {
         Americano americano = new Americano();
-        assertEquals(americano.getName(), "아메리카노");
+        assertEquals("아메리카노", americano.getName());
         assertThat(americano.getName()).isEqualTo("아메리카노");
     }
 
     @Test
     void getPrice() {
         Americano americano = new Americano();
-        assertEquals(americano.getPrice(), 4000);
+        assertEquals(4000, americano.getPrice());
         assertThat(americano.getPrice()).isEqualTo(4000);
     }
 }
